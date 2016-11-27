6f58cd423d8573753bbd322a629bd2e40a4bfc28
Denis Sadilo <Denis Sadilo>
2016-11-27 13:47:12 +0200

    server.js

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
-const config = require('./webpack.config');
+const config = process.env.NODE_ENV === 'production' ? require('./webpack.production.config') : require('./webpack.config');
 
 const app = express();
 const compiler = webpack(config);
 const port = process.env.PORT || process.env.port || 3001;
-app.use(/\.js$|\.css$/,express.static('dist'));
+app.use(/\.js$|\.css$/, express.static('dist'));
 
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: false,
-    publicPath: config.output.publicPath,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000 // is this the same as specifying --watch-poll?
-    }
-  }));
 
-  app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*',function(req,res,next) {
-  if(/\.js$|\.css$|\.png$|\.jpg$/.test(req.url)){
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: false,
+  publicPath: config.output.publicPath,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000 // is this the same as specifying --watch-poll?
+  }
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', function (req, res, next) {
+  if (/\.js$|\.css$|\.png$|\.jpg$/.test(req.url)) {
     const splittedUrl = req.url.split('/');
-    res.sendFile(path.join(__dirname,'./public',splittedUrl[splittedUrl.length-1]));
-  return;
+    res.sendFile(path.join(__dirname, './public', splittedUrl[splittedUrl.length - 1]));
+    return;
   }
   next();
 });
@@ -32,11 +34,11 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(port,function (err) {
+app.listen(port, function (err) {
   if (err) {
     console.error(err);
     return;
   }
 
-  console.log('Listening at port : '+port);
+  console.log('Listening at port : ' + port);
 });
